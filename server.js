74f33a73e48d0efe1d2b5a0b0840c302bdf8df8f
TonyBNK <borisenk-anton@yandex.ru>
2022-07-13 09:26:42 +0300

    Updated server.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.use((req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
