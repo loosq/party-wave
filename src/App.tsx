8cb60aadbefed5372d240d780e16052d1fc840fa
str <only-666@mail.ru>
2022-07-17 13:47:21 +0300

    added forum page

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import Navigation from './components/navigation/index';
 import Main from './components/pages/main/index';
 import Leaderboard from './components/pages/leaderboard/index';
+import Forum from './components/pages/forum/index';
 import './App.scss';
 
 const App = () => (
@@ -16,6 +17,7 @@ const App = () => (
             <Routes>
                 <Route path="/" element={<Main/>}/>
                 <Route path="/statistics" element={<Leaderboard/>}/>
+                <Route path="/forum" element={<Forum/>}/>
             </Routes>
         </Router>
     </div>
--- src/components/pages/leaderboard/Leaderboard.tsx
+++ src/components/pages/leaderboard/Leaderboard.tsx
@@ -5,17 +5,20 @@ import {title, header, data} from './config';
 const Leaderboard = () => (
     <div className={'leaderboard'}>
         <h1 className={'leaderboard__title'}>{title}</h1>
-        <table>
-            <thead>
-                <tr>
-                    {Array.isArray(header) && header.map((line) => <td
-                        key={line}>{line}</td>)}</tr>
-            </thead>
-            <tbody>
-                {Array.isArray(data) && data.map((row) => <tr key={row[0]}>{row.map((line) => <td
-                    key={line}>{line}</td>)}</tr>)}
-            </tbody>
-        </table>
+        <div className={'leaderboard__table table'}>
+            <div className={'table__row'}>
+                {Array.isArray(header) && header.map((line, i) => <span
+                    className={`table__row-col--${i + 1}`}
+                    key={line}>{line}</span>)}</div>
+            <div>
+                {Array.isArray(data) && data.map((row) => <div
+                    className={'table__row  table__row-col--data'}
+                    key={row[0]}>{row.map((line, i) => <span
+                        className={`table__row-col--${i + 1}`}
+                        key={line}
+                    >{line}</span>)}</div>)}
+            </div>
+        </div>
     </div>
 );
 
