514260f3b79add2c4feffbe13f3564c6a105acf7
Veniamin Ryadnov <veniamin.ryadnov@gmail.com>
2022-07-21 19:03:01 +0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function PrivateRoute() {
     return auth ? <Outlet /> : <Navigate to='/login' />;
 }
 
-function App() {
+export default function App() {
     return (
         <div className='container'>
             <Router>
